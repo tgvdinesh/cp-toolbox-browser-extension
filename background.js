519ab793d2ec5ba5a41c4f6a0c2316dfd8636774
Dinesh V <tgvdinesh@gmail.com>
2018-04-13 22:09:45 +0530

    Add sender detail in payload

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // Send a message to the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         payLoad.url = tabs[0].url;
+        payLoad.sender = parseTask(tabs[0]);
         chrome.tabs.sendMessage(tabs[0].id, {"message": "clicked_browser_action"});
     });
 });
@@ -36,5 +37,34 @@ function reload() {
 const URL = "http://127.0.0.1:4243";
 var payLoad = {
     "url": "https://www.hackerrank.com/challenges/30-bitwise-and/problem",
+    "sender": "",
     "htmlBody": "<html><head></head><body></body></html>"
 };
+
+function parseTask(tab) {
+    if (/^https:\/\/.*contest2?[.]yandex[.](ru|com)\/.*contest\/\d*\/problems.*$/.test(tab.url)) {
+        return 'yandex';
+    } else if (/^http:\/\/codeforces[.](ru|com)\/(contest|problemset|gym)\/(\d*\/problem|problem\/\d*)\/.+$/.test(tab.url)) {
+        return 'codeforces';
+    } else if (/^https:\/\/(www[.])?hackerrank[.]com.*$/.test(tab.url)) {
+        return 'hackerrank';
+    } else if (/^https:\/\/www[.]facebook[.]com\/hackercup\/problem\/\d+\/$/.test(tab.url)) {
+        return 'facebook';
+    } else if (/^http:\/\/(www[.])?usaco[.]org\/(current\/)?index[.]php[?]page[=]viewproblem.*$/.test(tab.url)) {
+        return 'usaco';
+    } else if (/^https:\/\/code[.]google[.]com\/codejam\/contest\/\d*\/dashboard.*$/.test(tab.url)) {
+        return 'gcj';
+    } else if (/^http:\/\/contest[.]bayan[.]ir\/en\/contest\/.*\/problem\/[A-Z]\/$/.test(tab.url)) {
+        return 'bayan';
+    } else if (/^https:\/\/.*[.]kattis[.]com\/(contests\/.+\/)?problems\/.*$/.test(tab.url)) {
+        return 'kattis';
+    } else if (/^https:\/\/(www[.])?codechef[.]com.*$/.test(tab.url)) {
+        return 'codechef';
+    } else if (/^https?:\/\/(www[.])?hackerearth[.]com\/(.*\/)?(algorithm|approximate)\/.*$/.test(tab.url)) {
+        return 'hackerearth';
+    } else if (/^http:\/\/.*[.]contest[.]atcoder[.]jp\/tasks\/.*$/.test(tab.url)) {
+        return 'atcoder';
+    } else if (/^https:\/\/csacademy[.]com\/contest\/.*\/task\/.*$/.test(tab.url)) {
+        return 'csacademy';
+    }
+}
